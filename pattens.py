ed5a1dde1f70e32481e5b9e7670dbf49414c4f10
Rakhim <rrakhimbekov@gmail.com>
2020-06-06 22:02:01 +0600

    Update pattens.py
    upd

--- pattens.py
+++ pattens.py
@@ -14,18 +14,22 @@ bad['address'] = bad['address'].str.replace('XI', '11')
 bad['address'] = bad['address'].str.replace('XII', '12')
 bad['address'] = bad['address'].str.replace('XIII', '13')
 
+bad['index'] = bad['address'].str.extract('([0-9][0-9][0-9]+)')
+bad['index'] = bad['index'].replace(np.nan, '', regex=True)
+bad['address'] = bad['address'].str.replace('([0-9][0-9][0-9]+)', '')
+
 bad['city'] = bad['address'].str.extract('(г\.?\ ?[А-Я][а-яА-Я-]+)')
 bad['city'] = bad['city'].replace(np.nan, '', regex=True)
 bad['address'] = bad['address'].str.replace('(г\.?\ ?[а-яА-Я-]+)', '')
 
-bad['hous'] = bad['address'].str.extract('(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)')
+bad['hous'] = bad['address'].str.extract('(д\.\ ?[0-9]+[а-яА-Я]?|дом\.?\ ?[0-9]+[а-яА-Я]?)')
 bad['hous'] = bad['hous'].replace(np.nan, '', regex=True)
-bad['address'] = bad['address'].str.replace('(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)', '')
+bad['address'] = bad['address'].str.replace('(д\.\ ?[0-9]+[а-яА-Я]?|дом\.?\ ?[0-9]+[а-яА-Я]?)', '')
 
 bad['favella'] = bad['address'].str.extract('(д\.\ ?[а-яА-Я-]+)')
 bad['favella'] = bad['favella'].replace(np.nan, '', regex=True)
 bad['address'] = bad['address'].str.replace('(д\.\ ?[а-яА-Я-]+)', '')
 
-bad['lane'] = bad['address'].str.extract('(пер\.?[еулок]*\ ?[^ ,]+)')
+bad['lane'] = bad['address'].str.extract('(пер\.?[еулок]*\ ?[^ ,]+|[а-яА-Я]+ ?пер\.?[еулок]*)')
 bad['lane'] = bad['lane'].replace(np.nan, '', regex=True)
-bad['address'] = bad['address'].str.replace('(пер\.?[еулок]*\ ?[^ ,]+)', '')
+bad['address'] = bad['address'].str.replace('(пер\.?[еулок]*\ ?[^ ,]+|[а-яА-Я]+ ?пер\.?[еулок]*)', '')
