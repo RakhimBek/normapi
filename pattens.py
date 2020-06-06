1b292ee9795073f38503a5fa54040819b114de6a
Rakhim <rrakhimbekov@gmail.com>
2020-06-06 18:52:33 +0600

    Update pattens.py

--- pattens.py
+++ pattens.py
@@ -1,7 +1,7 @@
 # city, house, favella
 
 bad = pd.read_csv('bad.csv', sep=';')
-bad['city'] = bad['address'].str.extract('(г\.\ ?[а-яА-Я]+)') # город
+bad['city'] = bad['address'].str.extract('(г\.\ ?[а-яА-Я-]+)') # город
 bad['house'] = bad['address'].str.extract('(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)') # дом
 bad['favella'] = bad['address'].str.extract('(д\.\ ?[а-яА-Я-]+)') # деревня
 
