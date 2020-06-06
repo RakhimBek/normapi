53cd2bd9006aff41e12731b23ae56dfa6bc7cbcf
Anton Dramoretskiy <DramoretskiyAS@sibedge.com>
2020-06-06 23:07:50 +0700

    added process

--- api/routers/normapi.py
+++ api/routers/normapi.py
@@ -1,9 +1,13 @@
 """Backup Cloud Storage module routings
 
 """
+import re
+import shutil
+from pathlib import Path
+import pandas as pd
+import numpy as np
 
 from fastapi import APIRouter, File, UploadFile
-
 
 norm_api = APIRouter()
 
@@ -20,4 +24,57 @@ async def create_file(file: bytes = File(...)):
 
 @norm_api.post("/api/file/upload/")
 async def create_upload_file(file: UploadFile = File(...)):
+    save_upload_file(file, Path(file.filename))
+    process(Path(file.filename))
     return {"filename": file.filename}
+
+
+def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
+    try:
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(upload_file.file, buffer)
+    finally:
+        upload_file.file.close()
+
+
+def process(destination: Path) -> None:
+    street = r'(ул\.?\s\w+(\s\w+)?|улица\s\w+(\s\w+)?|\w+(\s\w+)?\sулица|\w+(\s\w+)?\sул\.?)'
+    area = r'(обл\.?\s\w+|область\s\w+|\w+\sобласть|\w+\sобл\.?)'
+    bad = pd.read_csv('bad.csv', sep=';')
+    new_file = pd.DataFrame()
+    new_file['address'] = bad['address']
+    bad['address'] = bad['address'].str.replace('I', '1')
+    bad['address'] = bad['address'].str.replace('II', '2')
+    bad['address'] = bad['address'].str.replace('III', '3')
+    bad['address'] = bad['address'].str.replace('IV', '4')
+    bad['address'] = bad['address'].str.replace('V', '5')
+    bad['address'] = bad['address'].str.replace('VI', '6')
+    bad['address'] = bad['address'].str.replace('VII', '7')
+    bad['address'] = bad['address'].str.replace('VIII', '8')
+    bad['address'] = bad['address'].str.replace('IX', '9')
+    bad['address'] = bad['address'].str.replace('X', '10')
+    bad['address'] = bad['address'].str.replace('XI', '11')
+    bad['address'] = bad['address'].str.replace('XII', '12')
+    bad['address'] = bad['address'].str.replace('XIII', '13')
+
+    bad['city'] = bad['address'].str.extract(r'(г\.?\ ?[А-Я][а-яА-Я-]+)')
+    bad['city'] = bad['city'].replace(np.nan, '', regex=True)
+    bad['address'] = bad['address'].str.replace(r'(г\.?\ ?[а-яА-Я-]+)', '')
+
+    bad['hous'] = bad['address'].str.extract(r'(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)')
+    bad['hous'] = bad['hous'].replace(np.nan, '', regex=True)
+    bad['address'] = bad['address'].str.replace(r'(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)', '')
+
+    bad['favella'] = bad['address'].str.extract(r'(д\.\ ?[а-яА-Я-]+)')
+    bad['favella'] = bad['favella'].replace(np.nan, '', regex=True)
+    bad['address'] = bad['address'].str.replace(r'(д\.\ ?[а-яА-Я-]+)', '')
+
+    bad['lane'] = bad['address'].str.extract(r'(пер\.?[еулок]*\ ?[^ ,]+)')
+    bad['lane'] = bad['lane'].replace(np.nan, '', regex=True)
+    bad['address'] = bad['address'].str.replace(r'(пер\.?[еулок]*\ ?[^ ,]+)', '')
+
+    bad['area'] = bad['address'].str.extract(area)
+    bad['area'] = bad['area'].replace(np.nan, '', regex=True)
+    bad['address'] = bad['address'].str.replace(area, '')
+
+    new_file.to_csv('proba.csv')
--- pattens.py
+++ pattens.py
@@ -1,5 +1,11 @@
+import pandas as pd
+import numpy as np
+
 # city, house, favella
 
+street = r'(ул\.?\s\w+(\s\w+)?|улица\s\w+(\s\w+)?|\w+(\s\w+)?\sулица|\w+(\s\w+)?\sул\.?)'
+area = r'(обл\.?\s\w+|область\s\w+|\w+\sобласть|\w+\sобл\.?)'
+bad = pd.read_csv('bad.csv', sep=';')
 bad['address'] = bad['address'].str.replace('I', '1')
 bad['address'] = bad['address'].str.replace('II', '2')
 bad['address'] = bad['address'].str.replace('III', '3')
@@ -14,18 +20,26 @@ bad['address'] = bad['address'].str.replace('XI', '11')
 bad['address'] = bad['address'].str.replace('XII', '12')
 bad['address'] = bad['address'].str.replace('XIII', '13')
 
-bad['city'] = bad['address'].str.extract('(г\.?\ ?[А-Я][а-яА-Я-]+)')
+bad['city'] = bad['address'].str.extract(r'(г\.?\ ?[А-Я][а-яА-Я-]+)')
 bad['city'] = bad['city'].replace(np.nan, '', regex=True)
-bad['address'] = bad['address'].str.replace('(г\.?\ ?[а-яА-Я-]+)', '')
+bad['address'] = bad['address'].str.replace(r'(г\.?\ ?[а-яА-Я-]+)', '')
 
-bad['hous'] = bad['address'].str.extract('(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)')
+bad['hous'] = bad['address'].str.extract(r'(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)')
 bad['hous'] = bad['hous'].replace(np.nan, '', regex=True)
-bad['address'] = bad['address'].str.replace('(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)', '')
+bad['address'] = bad['address'].str.replace(r'(д\.\ ?[0-9]+[а-яА-Я]?|дом\ ?[0-9]+[а-яА-Я]?)', '')
 
-bad['favella'] = bad['address'].str.extract('(д\.\ ?[а-яА-Я-]+)')
+bad['favella'] = bad['address'].str.extract(r'(д\.\ ?[а-яА-Я-]+)')
 bad['favella'] = bad['favella'].replace(np.nan, '', regex=True)
-bad['address'] = bad['address'].str.replace('(д\.\ ?[а-яА-Я-]+)', '')
+bad['address'] = bad['address'].str.replace(r'(д\.\ ?[а-яА-Я-]+)', '')
 
-bad['lane'] = bad['address'].str.extract('(пер\.?[еулок]*\ ?[^ ,]+)')
+bad['lane'] = bad['address'].str.extract(r'(пер\.?[еулок]*\ ?[^ ,]+)')
 bad['lane'] = bad['lane'].replace(np.nan, '', regex=True)
-bad['address'] = bad['address'].str.replace('(пер\.?[еулок]*\ ?[^ ,]+)', '')
+bad['address'] = bad['address'].str.replace(r'(пер\.?[еулок]*\ ?[^ ,]+)', '')
+
+bad['street'] = bad['address'].str.extract(street)
+bad['street'] = bad['street'].replace(np.nan, '', regex=True)
+bad['address'] = bad['address'].str.replace(street, '')
+
+bad['area'] = bad['address'].str.extract(area)
+bad['area'] = bad['area'].replace(np.nan, '', regex=True)
+bad['address'] = bad['address'].str.replace(area, '')
