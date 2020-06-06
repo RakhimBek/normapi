bc42f1387426da1735aa4202320f2de303e56367
Rakhimbekov R. R <rakhimbekov.r.rakh@sberbank.ru>
2020-06-06 20:40:25 +0600

    to POST (upload file)

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ app.add_middleware(
 def home():
     return {"message":"Hello, Man!"}
 
-@app.put("api/file/")
+@app.post("api/file/")
 async def create_file(file: bytes = File(...)):
     return {"file_size": len(file)}
+
+@app.post("/api/file/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
