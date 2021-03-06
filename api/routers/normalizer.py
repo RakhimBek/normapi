425ca5b86ae6e3a26d7ceba62d2b9157d7c68af6
Raimbek <rrakhimbekov@gmail.com>
2021-03-06 13:46:32 +0600

    refactor

--- api/routers/normalizer.py
+++ api/routers/normalizer.py
@@ -1,13 +1,12 @@
-import shutil
 import json
-import uuid
 import os
-import requests
-import pandas as pd
+import shutil
 import subprocess
 import time
-
+import uuid
 from pathlib import Path
+
+import requests
 from fastapi import APIRouter, File, UploadFile
 from starlette.responses import FileResponse
 
@@ -15,90 +14,90 @@ normalizer = APIRouter()
 
 
 def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
-    try:
-        with destination.open('wb') as buffer:
-            shutil.copyfileobj(upload_file.file, buffer)
-    finally:
-        upload_file.file.close()
+	try:
+		with destination.open('wb') as buffer:
+			shutil.copyfileobj(upload_file.file, buffer)
+	finally:
+		upload_file.file.close()
 
 
 def call(model_name, name):
-    subprocess.run(
-        f'onmt_translate -batch_size 10 -beam_size 20 -model ../models/{model_name}/model.pt \
+	subprocess.run(
+		f'onmt_translate -batch_size 10 -beam_size 20 -model ../models/{model_name}/model.pt \
            -src {name} -output {model_name}{name} -min_length 0 \
            -stepwise_penalty -coverage_penalty summary -beta 5 -length_penalty wu -alpha 0.9  \
            -block_ngram_repeat 0 -ignore_when_blocking "." "<t>" "</t>"',
-        shell=True
-    )
-    print(f'call {model_name}')
+		shell=True
+	)
+	print(f'call {model_name}')
 
 
 def wait_for_files(good_filepath):
-    while not os.path.exists(f'gorod{good_filepath}'):
-        time.sleep(1)
+	while not os.path.exists(f'gorod{good_filepath}'):
+		time.sleep(1)
 
-    while not os.path.exists(f'rayon{good_filepath}'):
-        time.sleep(1)
+	while not os.path.exists(f'rayon{good_filepath}'):
+		time.sleep(1)
 
-    while not os.path.exists(f'street{good_filepath}'):
-        time.sleep(1)
-    print('wait_for_files')
+	while not os.path.exists(f'street{good_filepath}'):
+		time.sleep(1)
+	print('wait_for_files')
 
 
 @normalizer.post("/api/normalizer/file/")
 async def create_upload_file(file: UploadFile = File(...)):
-    """Loads the passed file and performs processing.
+	"""Loads the passed file and performs processing.
 
-    Args:
-        file: A csv file that has an 'address' field that contains data that needs to be preprocessed.
+	Args:
+		file: A csv file that has an 'address' field that contains data that needs to be preprocessed.
 
-    Returns:
-        A new file with the original strings and the result of processing.
+	Returns:
+		A new file with the original strings and the result of processing.
 
-    """
-    print('/api/file/upload/')
+	"""
+	print('/api/file/upload/')
 
-    try:
-        suffix = str(uuid.uuid4()).replace('-', '').upper()
-        good_filepath = suffix + '.csv'
-        save_upload_file(file, Path(good_filepath))
+	try:
+		suffix = str(uuid.uuid4()).replace('-', '').upper()
+		good_filepath = suffix + '.csv'
+		save_upload_file(file, Path(good_filepath))
 
-        call('gorod', good_filepath)
-        call('rayon', good_filepath)
-        call('street', good_filepath)
+		call('gorod', good_filepath)
+		call('rayon', good_filepath)
+		call('street', good_filepath)
 
-        wait_for_files(good_filepath)
+		wait_for_files(good_filepath)
 
-        subprocess.run(
-            f'paste gorod{good_filepath} rayon{good_filepath} street{good_filepath} \
+		subprocess.run(
+			f'paste gorod{good_filepath} rayon{good_filepath} street{good_filepath} \
              | sed "s/[a-z<>/]//g" > res{good_filepath}',
-            shell=True
-        )
+			shell=True
+		)
 
-        # data = pd.read_csv(good_filepath, delimiter=';', header=None)
-        # values = list(map(lambda x: search_in_fias(x[0]), data.loc[:, [0]].values))
-        # pd.DataFrame(data=values).to_csv('res' + suffix + '.csv', sep=';', index=False, header=False)
+		# data = pd.read_csv(good_filepath, delimiter=';', header=None)
+		# values = list(map(lambda x: search_in_fias(x[0]), data.loc[:, [0]].values))
+		# pd.DataFrame(data=values).to_csv('res' + suffix + '.csv', sep=';', index=False, header=False)
 
-        return FileResponse('res' + good_filepath)
-        # return FileResponse(f'gorod{good_filepath}')
+		return FileResponse('res' + good_filepath)
+	# return FileResponse(f'gorod{good_filepath}')
 
-    except Exception as e:
-        print(e)
-        # return {"status": "bad"}
-        return None
+	except Exception as e:
+		print(e)
+		# return {"status": "bad"}
+		return None
 
 
 def search_in_fias(text):
-    url = 'https://fias.nalog.ru/Search/Searching?text=' + text
+	url = 'https://fias.nalog.ru/Search/Searching?text=' + text
 
-    payload = {}
-    headers = {
-        'mode': 'cors'
-    }
+	payload = {}
+	headers = {
+		'mode': 'cors'
+	}
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+	response = requests.request("GET", url, headers=headers, data=payload)
 
-    try:
-        return json.loads(response.text)[0]['PresentRow']
-    except Exception as e:
-        return 'No data found.'
+	try:
+		return json.loads(response.text)[0]['PresentRow']
+	except Exception as e:
+		return 'No data found.'
