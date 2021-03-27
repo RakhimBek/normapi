b41dac6326324bf6e4f7b51c18224a007f737b95
Rakhim <rrakhimbekov@gmail.com>
2021-03-27 13:16:01 +0600

    generate environment.properties

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import uvicorn
-from fastapi import FastAPI, File, UploadFile
-from starlette.staticfiles import StaticFiles
-from starlette.middleware.cors import CORSMiddleware
-from api.api import router as api_router
-
-from settings import HOST, PORT, PROJECT_NAME, DEBUG, API_VERSION, API_PREFIX, OAS_FILENAME
 from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+
+from api.api import router as api_router
+from settings import API_PREFIX, API_VERSION, DEBUG, HOST, OAS_FILENAME, PORT, PROJECT_NAME
 
 app = FastAPI(
     title=PROJECT_NAME,
@@ -32,4 +33,12 @@ app.add_middleware(
 app.include_router(api_router)
 
 if __name__ == '__main__':
+
+    with open('environment.properties', 'w+') as f:
+        f.write('HOST=213.159.209.30')
+        f.write('PORT=5432')
+        f.write('DATABASE=addresses')
+        f.write('USER=normalizer')
+        f.write('PASSWORD=1q3w2e4r5t')
+
     uvicorn.run(app, host=HOST, port=PORT)
